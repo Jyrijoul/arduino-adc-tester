98c16fa299783c15bd00c8949b3f599da8ebd715
Jüri Jõul <jyri.joul@gmail.com>
2021-12-17 22:17:46 +0200

    Perform refactoring and documenting

--- tester/ni_usb_6211.py
+++ tester/ni_usb_6211.py
@@ -1,11 +1,11 @@
-from output_device import OutputDeviceInterface
+import time
 import numpy as np
 import nidaqmx
+from nidaqmx import constants
+from output_device import OutputDeviceInterface
 from nidaqmx.constants import TerminalConfiguration
 from nidaqmx.stream_writers import AnalogSingleChannelWriter
 from nidaqmx.stream_readers import AnalogMultiChannelReader
-from nidaqmx.stream_readers import AnalogSingleChannelReader
-import time
 
 
 class NiUsb6211(OutputDeviceInterface):
@@ -18,31 +18,53 @@ class NiUsb6211(OutputDeviceInterface):
         output_channel="ao0",
         output_read_channel="ai0",
         vcc_read_channel="ai1",
+        output_pulse_channel="ao1",
         reference_voltage_estimate=5.0,
+        min_value=0.0,
+        max_value=5.0,
         timeout=1.0,
         verbose=True,
     ) -> None:
         self.device_name = device_name
         self.output_channel = output_channel
         self.output_read_channel = output_read_channel
+        self.output_pulse_channel = output_pulse_channel
         self.vcc_read_channel = vcc_read_channel
         self.vcc_reading = reference_voltage_estimate
+        self.min_value = min_value
+        self.max_value = max_value
         self.timeout = timeout
         self.verbose = verbose
 
     def find_devices() -> list[str]:
+        """Finds the NI DAQ devices present in the system."""
         system = nidaqmx.system.System.local()
         return [device.name for device in system.devices]
 
     def error_handler(self, err):
+        """A custom error handler in order to always properly close the opened tasks.
+        
+        Raises
+        ------
+        Exception
+            Raises the same exception it receives from any other method.
+
+        """
         self.write_task.close()
         self.read_task.close()
         raise err
 
-    def init(self):
+    def init(self, mode="software") -> None:
+        """Initializes all the required tasks.
+
+        Parameters
+        ----------
+        mode : str, optional
+            Whether to use software or hardware triggering.
+            Possible values are "software" and "hardware" (default).
+        """
+
         terminal_config = TerminalConfiguration.RSE
-        min_val = -10
-        max_val = 10
 
         try:
             self.write_task = nidaqmx.Task("write_task")
@@ -52,36 +74,66 @@ class NiUsb6211(OutputDeviceInterface):
                 print("Created writing and reading tasks.")
 
             self.write_task.ao_channels.add_ao_voltage_chan(
-                f"{self.device_name}/{self.output_channel}"
+                f"{self.device_name}/{self.output_channel}",
+                min_val=self.min_value,
+                max_val=self.max_value,
             )
+
+            if mode == "hardware":
+                self.write_task.ao_channels.add_ao_voltage_chan(
+                    f"{self.device_name}/{self.output_pulse_channel}",
+                    min_val=self.min_value,
+                    max_val=self.max_value,
+                )
 
             self.read_task.ai_channels.add_ai_voltage_chan(
                 f"{self.device_name}/{self.output_read_channel}",
-                min_val=min_val,
-                max_val=max_val,
+                min_val=self.min_value,
+                max_val=self.max_value,
                 terminal_config=terminal_config,
             )
 
             self.read_task.ai_channels.add_ai_voltage_chan(
                 f"{self.device_name}/{self.vcc_read_channel}",
-                min_val=min_val,
-                max_val=max_val,
+                min_val=self.min_value,
+                max_val=self.max_value,
                 terminal_config=terminal_config,
             )
 
             if self.verbose:
                 print("Added required analog output and input channels.")
 
-            # task.timing.cfg_samp_clk_timing(1000, samps_per_chan=10000)
-            self.writer = AnalogSingleChannelWriter(self.write_task.out_stream)
-            self.reader = AnalogMultiChannelReader(self.read_task.in_stream)
+            if mode != "hardware":
+                # task.timing.cfg_samp_clk_timing(1000, samps_per_chan=10000)
+                self.writer = AnalogSingleChannelWriter(self.write_task.out_stream)
+                self.reader = AnalogMultiChannelReader(self.read_task.in_stream)
 
             if self.verbose:
                 print("Created writing and reading streams.")
         except Exception as e:
             self.error_handler(e)
 
-    def read_samples(self, n, channels: list[int] = [0, 1]) -> np.ndarray:
+    def read_samples(self, n: int, channels: list[int] = [0, 1]) -> np.ndarray:
+        """Read N samples from the Arduino.
+
+        Parameters
+        ----------
+        n : int
+            The amount of samples to be read.
+        channels: int or list[int], optional, default: [0, 1]
+            For specifying which channels to read.
+            0 corresponds to the current output, 1 to the reference voltage.
+            Both a single integer or a list of one or two can be used.
+            The default parameter reads both.
+
+        Returns
+        -------
+        np.ndarray of floats
+            With the shape of either 
+            (2, n) when both channels are specified or
+            (n,) when only one channel is specified.
+        """
+
         try:
             # self.read_task.start()
             data = np.zeros((2, n))
@@ -91,8 +143,25 @@ class NiUsb6211(OutputDeviceInterface):
         except Exception as e:
             self.error_handler(e)
 
-    def write_sample(self, value: float):
-        # Enter the critical section, where an exception 
+    def write_sample(self, value: float, read=True):
+        """Outputs a desired voltage.
+
+        Optionally also reads both the reference voltage
+        as well as the current output voltage.
+        
+        Parameters
+        ----------
+        value : float
+            The output voltage.
+        read : bool, optional, default: True
+            If True, also reads both the reference voltage
+            as well as the current output voltage. Both of their values
+            can be accessed by separate methods named "get_reference_voltage"
+            and "get_measured_output_voltage", respectively.
+            Also, pass "update = False" for both of them
+            to directly get the values read and saved in this method.
+        """
+        # Enter the critical section, where an exception
         # indicates the need to deinitialize the tasks.
         try:
             # Try whether the value is a float or not.
@@ -102,24 +171,84 @@ class NiUsb6211(OutputDeviceInterface):
             self.writer.write_one_sample(value)
 
             # Read the written voltage and VCC.
-            samples = self.read_samples(1)
-            
+            if read:
+                samples = self.read_samples(1)
+
         except Exception as e:
             self.error_handler(e)
 
         # Also read the VCC for reference, and the output voltage for validation.
         # These can be subsequently used by other methods.
-        self.output_reading = samples[0, 0]
-        self.vcc_reading = samples[1, 0]
+        if read:
+            self.output_reading = samples[0, 0]
+            self.vcc_reading = samples[1, 0]
         # print(f"Output = {self.output_reading}, VCC = {self.vcc_reading}.")
-    
-    def get_reference_voltage(self):
+
+    def write_samples_clocked(self, samples: np.ndarray, sample_rate: float) -> None:
+        # Enter the critical section, where an exception
+        # indicates the need to deinitialize the tasks.
+        try:
+            n_samples = len(samples)
+            sample_rate *= 2
+
+            # 2x the samples because for each real sample,
+            # we want to output both 0 and 1 on the other channel to create a pulse train.
+            output_samples = np.zeros((2, n_samples * 2))
+            output_samples[0] = np.repeat(samples, 2)
+            # 1 --> 0 == falling edge which the Arduino captures.
+            output_samples[1] = np.tile([5, 0], n_samples)
+            print(output_samples.shape)
+
+            print(output_samples)
+
+            # self.write_task.timing.cfg_samp_clk_timing(sample_rate, sample_mode=constants.AcquisitionType.CONTINUOUS)
+            self.write_task.timing.cfg_samp_clk_timing(
+                sample_rate,
+                sample_mode=constants.AcquisitionType.FINITE,
+                samps_per_chan=(n_samples * 2),
+            )
+            samples_written = self.write_task.write(output_samples, auto_start=True)
+
+            print(f"{samples_written}/{n_samples} samples written.")
+        except Exception as e:
+            self.error_handler(e)
+
+    def get_reference_voltage(self, update=True):
+        """Reads and returns the Arduino's reference voltage.
+        
+        Parameters
+        ----------
+        update : bool, optional, default: True
+            Whether to get a new reading or just to return the previously read value.
+            This option works well in tandem with the 
+            "read" parameter of the method "write_sample".
+        """
+
+        if update:
+            # Read the written voltage and VCC.
+            self.vcc_reading = self.read_samples(1, 1)
+
         return self.vcc_reading
 
-    def get_measured_output_voltage(self):
+    def get_measured_output_voltage(self, update=True):
+        """Reads and returns the current output voltage.
+        
+        Parameters
+        ----------
+        update : bool, optional, default: True
+            Whether to get a new reading or just to return the previously read value.
+            This option works well in tandem with the 
+            "read" parameter of the method "write_sample".
+        """
+
+        if update:
+            # Read the written voltage and VCC.
+            self.output_reading = self.read_samples(1, 0)
+
         return self.output_reading
 
     def deinit(self):
+        """Properly closes all the opened tasks."""
         if self.verbose:
             print("Closing the tasks.")
         self.write_task.close()
@@ -127,16 +256,34 @@ class NiUsb6211(OutputDeviceInterface):
 
 
 if __name__ == "__main__":
-    niUsb6211 = NiUsb6211()
-    print(NiUsb6211.find_devices())
-    niUsb6211.init()
+    test = "hardware_trigger"
 
-    niUsb6211.write_sample(3.3)
-    samples = niUsb6211.read_samples(10, channels=[0, 1])
-    print(samples, samples.shape)
+    if test == "software_trigger":
+        niUsb6211 = NiUsb6211()
+        print(NiUsb6211.find_devices())
+        niUsb6211.init()
 
-    samples = niUsb6211.read_samples(1, channels=0)
-    print(samples, samples.shape)
-    print(niUsb6211.get_reference_voltage(), niUsb6211.get_measured_output_voltage())
-    niUsb6211.write_sample(0)
-    niUsb6211.deinit()
+        niUsb6211.write_sample(3.3)
+        samples = niUsb6211.read_samples(10, channels=[0, 1])
+        print(samples, samples.shape)
+
+        samples = niUsb6211.read_samples(1, channels=0)
+        print(samples, samples.shape)
+        print(
+            niUsb6211.get_reference_voltage(), niUsb6211.get_measured_output_voltage()
+        )
+        niUsb6211.write_sample(0)
+        niUsb6211.deinit()
+    else:
+        niUsb6211 = NiUsb6211(output_pulse_channel="ao1")
+        print(NiUsb6211.find_devices())
+        niUsb6211.init()
+
+        n_samples = 8192
+        output_data = np.linspace(0, 4.6, n_samples)
+
+        sample_rate = 8192
+        niUsb6211.write_samples_clocked(output_data, sample_rate)
+
+        time.sleep(n_samples / sample_rate + 0.1)
+        niUsb6211.deinit()
--- tester/serial_device.py
+++ tester/serial_device.py
@@ -1,5 +1,5 @@
 import serial
-from serial.serialutil import SerialException
+from serial.serialutil import SerialException, SerialTimeoutException
 import serial.tools.list_ports
 from input_device import InputDeviceInterface
 
@@ -10,14 +10,35 @@ class SerialDevice(InputDeviceInterface):
     baud_rate = 115200
     verbose = True
 
-    def __init__(self, timeout=1, serial_port="", baud_rate: int = 2000000, verbose=True) -> None:
+    def __init__(
+        self, timeout=1, serial_port="", baud_rate: int = 2000000, verbose=True
+    ) -> None:
         self.timeout = timeout
         self.serial_port = serial_port
         self.baud_rate = baud_rate
         self.verbose = verbose
         # super().__init__()
 
-    def init(self, device_name="CH340") -> None:
+    def init(self, mode="software", device_name="CH340") -> None:
+        """Establishes the serial connection.
+
+        Parameters
+        ----------
+        mode : str, optional
+            Whether to use software or hardware triggering.
+            Possible values are "software" (default) and "hardware".
+        device_name : str, optional
+            Provides the device's name to be searched for.
+            "CH340" by default (Arduino Nano).
+
+        Raises
+        ------
+        SerialException
+            If the connection cannot be established.
+        """
+
+        self.mode = mode
+
         if self.serial_port == "":
             # Serial devices (Arduinos)
             serial_devices = serial.tools.list_ports.comports()
@@ -25,13 +46,18 @@ class SerialDevice(InputDeviceInterface):
             if self.verbose:
                 print("Available serial devices:")
             for device in serial_devices:
-                if self.verbose:print("\t", device.device,
-                      "(name matches)" if device_name in device.description else "")
+                if self.verbose:
+                    print(
+                        "\t",
+                        device.device,
+                        "(name matches)" if device_name in device.description else "",
+                    )
                 if device_name in device.description:
                     self.serial_port = device.device
 
         self.ser = serial.Serial(
-            port=self.serial_port, baudrate=self.baud_rate, timeout=self.timeout)
+            port=self.serial_port, baudrate=self.baud_rate, timeout=self.timeout
+        )
         if self.verbose:
             print(f"Serial port {self.ser.port} opened.")
 
@@ -40,13 +66,23 @@ class SerialDevice(InputDeviceInterface):
             if self.verbose:
                 print("Trying to establish communication.", end="")
             conn_success = False
+
+            if mode == "software":
+                text_to_send = "s"
+            elif mode == "hardware":
+                text_to_send = "h"
+            else:
+                text_to_send = "s"  # Default
+
+            # Start sending data.
             for i in range(10):
-                self.ser.write("0".encode())
+                self.ser.write(text_to_send.encode())
                 self.ser.flushOutput()
                 value = self.ser.readline()
                 if self.verbose:
                     print(".", end="")
                 if value != b"":
+                    print(value.decode().strip())
                     conn_success = True
                     break
 
@@ -61,27 +97,146 @@ class SerialDevice(InputDeviceInterface):
             self.ser.close()
             raise e
 
-    def read_sample(self, char_to_write: str = "a") -> int:
-        try:
-            self.ser.write(char_to_write.encode())
-            self.ser.flushOutput()
-            value = self.ser.readline().decode().strip()
-            return int(value)
-        except Exception as e:
+    def read_sample(self):
+        """Requests one sample from the Arduino.
+        
+        Raises
+        ------
+        RuntimeError
+            When called with hardware triggering.
+        """
+
+        return self.read()
+
+    def read_vcc(self):
+        """Requests the Arduino to read its own VCC.
+        
+        Raises
+        ------
+        RuntimeError
+            When called with hardware triggering.
+        """
+
+        return self.read("v")
+
+    def read(self, char_to_write: str = "a") -> int:
+        """A general function to request a sample from the Arduino.
+        
+        Only to be used with software triggering.
+
+        Raises
+        ------
+        RuntimeError
+            When called with hardware triggering.
+        """
+        if self.mode == "software":
+            try:
+                self.ser.write(char_to_write.encode())
+                self.ser.flushOutput()
+                value = self.ser.readline().decode().strip()
+                return int(value)
+            except Exception as e:
+                self.ser.close()
+                raise e
+        else:
             self.ser.close()
-            raise e
+            raise RuntimeError(
+                "The serial device needs to be initialized in software triggering mode."
+            )
+
+    def read_sample_clocked(self) -> int:
+        """Waits for the Arduino to send a sample.
+        
+        Arduino reads and sends the sample when a falling edge 
+        on the specified pin (D2 by default) is detected.
+
+        When a specified timeout is received, -1 is returned.
+
+        Raises
+        ------
+        RuntimeError
+            When called with software triggering.
+        """
+
+        if self.mode == "hardware":
+            try:
+                value = self.ser.readline().decode().strip()
+                if value == "":
+                    # Return a non-valid result when timeout exceeded.
+                    print("Serial timeout reached!")
+                    return -1
+
+                return int(value)
+            except Exception as e:
+                self.ser.close()
+                raise e
+        else:
+            self.ser.close()
+            raise RuntimeError(
+                "The serial device needs to be initialized in hardware triggering mode."
+            )
+
+    # TODO: Figure out whether this function is needed at all.
+    def read_samples_clocked(self, n_samples) -> int:
+        """Reads the requested amount of samples from the Arduino.
+        
+        Arduino reads and sends a sample when a falling edge 
+        on the specified pin (D2 by default) is detected.
+
+        When a specified timeout is received, 
+        -1 is used as that sample's value.
+
+        Raises
+        ------
+        RuntimeError
+            When called with software triggering.
+        """
+        
+        if self.mode == "hardware":
+            samples_read = 0
+            samples = []
+            try:
+                while samples_read < n_samples:
+                    value = self.ser.readline().decode().strip()
+                    if value == "":
+                        # Return a non-valid result when timeout exceeded.
+                        print("Serial timeout reached!")
+                        continue
+                    else:
+                        # samples.append(int(value))
+                        print(value)
+                        samples_read += 1
+
+                return samples
+            except Exception as e:
+                self.ser.close()
+                raise e
+        else:
+            self.ser.close()
+            raise RuntimeError(
+                "The serial device needs to be initialized in hardware triggering mode."
+            )
 
     def deinit(self) -> None:
+        """Closes the serial connection."""
         self.ser.close()
         if self.verbose:
             print("Serial connection closed.")
 
 
 if __name__ == "__main__":
-    serialDevice = SerialDevice()
+    serialDevice = SerialDevice(timeout=2)
     print(issubclass(SerialDevice, InputDeviceInterface))
 
-    serialDevice.init()
-    for i in range(100):
-        print(serialDevice.read_sample())
-    serialDevice.deinit()
+    triggering = "hardware"
+
+    if triggering == "software":
+        serialDevice.init()
+        for i in range(100):
+            print(serialDevice.read_sample())
+        serialDevice.deinit()
+    else:
+        serialDevice.init("hardware")
+        for i in range(100):
+            print(serialDevice.read_sample_clocked())
+        serialDevice.deinit()
